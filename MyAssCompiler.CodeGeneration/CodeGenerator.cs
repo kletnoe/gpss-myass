356bc5b663f2149bc1ecd80b0452952e6e7886df
FCRnah <none@none>
2014-05-09 18:49:45 +0300

    - MyAssCompiler.CodeGen skeleton & some tests

---
+++ MyAssCompiler.CodeGeneration/CodeGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace MyAssCompiler
+{
+    public class CodeGenerator
+    {
+        private Dictionary<string, List<double>> ast = new Dictionary<string, List<double>>();
+
+        public CodeGenerator()
+        {
+            this.ast.Add("Generate", new List<double> { 10 });
+        }
+
+        public void Some(string moduleName = "Test.exe")
+        {
+            AssemblyName assemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(moduleName));
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(moduleName, true);
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("MyAssGenerated.Model", TypeAttributes.Public);
+
+            typeBuilder.DefineField("simulationObject", typeof(String), FieldAttributes.Private);
+
+
+            ILGenerator il;
+
+            MethodBuilder createSimpleMethod = typeBuilder.DefineMethod(
+                "SimpleMethod", MethodAttributes.Private | MethodAttributes.Static, typeof(double), new Type[0]);
+            il = createSimpleMethod.GetILGenerator();
+            LocalBuilder val = il.DeclareLocal(typeof(Int32));
+            val.SetLocalSymInfo("block1");
+            il.Emit(OpCodes.Ldc_I4, 5);
+            il.Emit(OpCodes.Stloc, val);
+            il.Emit(OpCodes.Call, typeof(System.Console).GetMethod("WriteLine", new System.Type[] {}));
+            il.Emit(OpCodes.Ldloc, val);
+            //il.Emit(OpCodes.Ldloc, block);
+            il.Emit(OpCodes.Ret);
+
+            MethodBuilder createBlocksMethod = typeBuilder.DefineMethod(
+                "DefineBlocks", MethodAttributes.Private | MethodAttributes.Static, typeof(double), new Type[0]);
+            il = createBlocksMethod.GetILGenerator();
+            LocalBuilder block = il.DeclareLocal(typeof(MyAssFramework.Blocks.Generate));
+            block.SetLocalSymInfo("block1");
+            il.Emit(OpCodes.Ldc_I4, 5);
+            il.Emit(OpCodes.Stloc, block);
+            il.Emit(OpCodes.Call, typeof(System.Console).GetMethod("WriteLine", new System.Type[] { }));
+            il.Emit(OpCodes.Ldloc, block);
+            //il.Emit(OpCodes.Ldloc, block);
+            il.Emit(OpCodes.Ret);
+
+            MethodBuilder mainMethod = typeBuilder.DefineMethod(
+                "Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), System.Type.EmptyTypes);
+            il = mainMethod.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "Test!");
+            il.Emit(OpCodes.Call, typeof(System.Console).GetMethod("WriteLine", new System.Type[] { typeof(string) }));
+            il.EmitCall(OpCodes.Call, createBlocksMethod, new Type[0]);
+            il.Emit(OpCodes.Ret);
+
+
+
+            typeBuilder.CreateType();
+            moduleBuilder.CreateGlobalFunctions();
+            assemblyBuilder.SetEntryPoint(mainMethod);
+            assemblyBuilder.Save(moduleName);
+        }
+    }
+}
---
+++ MyAssCompiler.CodeGeneration/MetadataRetriever.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace MyAssCompiler.CodeGeneration
+{
+    public class MetadataRetriever
+    {
+        public string AssemblyName { get { return "MyAssFramework"; } }
+        public string AssemblyDllName { get { return this.AssemblyName + ".dll"; } }
+        
+        public IEnumerable<Type> GetMyAssFrameworkBlockTypes()
+        {
+            return Assembly.LoadFrom(this.AssemblyDllName).GetTypes().Where(x => !x.IsInterface && !x.IsAbstract)
+                .Where(x => x.Namespace == this.AssemblyName + ".Blocks");
+        }
+
+        public IEnumerable<Type> GetMyAssFrameworkCommandTypes()
+        {
+            return Assembly.LoadFrom(this.AssemblyDllName).GetTypes().Where(x => !x.IsInterface && !x.IsAbstract)
+                .Where(x => x.Namespace == this.AssemblyName + ".Commands");
+        }
+
+        public Type GetMyAssFrameworkSNAType()
+        {
+            return Assembly.LoadFrom(this.AssemblyDllName).GetType(this.AssemblyName + ".SNA.SNA");
+        }
+
+        public IEnumerable<MethodInfo> GetMyAssFrameworkSNAMethods()
+        {
+            return this.GetMyAssFrameworkSNAType().GetMethods().Where(x => x.IsStatic);
+        }
+
+        public IEnumerable<string> GetAllDefinitions()
+        {
+            return this.GetMyAssFrameworkBlockTypes().Select(x => x.Name)
+                .Concat(this.GetMyAssFrameworkCommandTypes().Select(x => x.Name))
+                .Concat(this.GetMyAssFrameworkSNAMethods().Select(x => x.Name))
+                ;
+        }
+    }
+}
