358cb4361ff9dcae92c8a306629a1a5950312306
FCRnah <none@none>
2014-06-17 17:12:43 +0300

    - ExampleVisitors

--- MyAssApplication/Program.cs
+++ MyAssApplication/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MyAssCompiler.AST;
 using MyAssCompiler;
 using MyAssCompiler.CodeGeneration;
+using System.CodeDom;
 
 namespace MyAssApplication
 {
@@ -12,21 +13,6 @@ namespace MyAssApplication
     {
         static void Main(string[] args)
         {
-            //List<String> pizda = new List<String>();
-            //pizda.Add("a");
-            //pizda.Add("b");
-            //pizda.Add("c");
-            //pizda.Add("d");
-            //pizda.Add("e");
-
-            //List<String> subPizda = pizda.GetRange(2, 2);
-            //subPizda.Clear();
-
-            //foreach (var item in pizda)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
             //foreach (var item in new MetadataRetriever().GetAllDefinitions())
             //{
             //    Console.WriteLine(item); 
@@ -37,8 +23,8 @@ namespace MyAssApplication
             //Console.WriteLine(model.ToString());
 
 
-            TestCodeGen.Run();
-            Console.WriteLine();
+            //TestCodeGen.Run();
+            //Console.WriteLine();
             //Model.RunDefaultModel();
             //Compiler.TestCodeGen();
         }
