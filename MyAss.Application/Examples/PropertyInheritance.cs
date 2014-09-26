c6b652bffa3d3db86eac6de40005d8aeeefb0dc7
FCRnah <none@none>
2014-09-26 11:55:39 +0300

    - Orthodox prop inheritance

---
+++ MyAss.Application/Examples/PropertyInheritance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAss.Application.Examples
+{
+    interface I
+    {
+        int MyProperty { get; }
+    }
+
+    abstract class A : I
+    {
+        public abstract int MyProperty { get; protected set; }
+    }
+
+    class C : A
+    {
+        public override int MyProperty { get; protected set; }
+    }
+}
