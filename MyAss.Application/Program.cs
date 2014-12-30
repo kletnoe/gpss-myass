6633abd94f5c24ea82f4af2791445bd3cabaa2a0
FCRnah <none@none>
2014-12-30 18:51:08 +0200

    - Removed v1 compiler stuff

--- MyAss.Application/Program.cs
+++ MyAss.Application/Program.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MyAss.Compiler.AST;
 using MyAss.Compiler;
-using MyAss.Compiler.CodeGeneration;
 using System.CodeDom;
 using CompiledTestModels;
 using MyAss.Application.Examples.NakedTestModels;
