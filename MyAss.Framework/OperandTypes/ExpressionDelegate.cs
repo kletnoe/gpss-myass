ab04b7da36966f7a97174992010837b0f161c7ae
FCRnah <none@none>
2015-06-10 19:35:01 +0300

    - Removed unused staff for bsc
    --HG--
    branch : bsc

--- MyAss.Framework/OperandTypes/ExpressionDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace MyAss.Framework.OperandTypes
-{
-    public delegate double ExpressionDelegate();
-}
